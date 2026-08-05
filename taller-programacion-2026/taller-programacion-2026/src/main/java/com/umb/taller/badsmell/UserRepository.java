0659355840295351172d0b6ff5e373cee4f52b1a
mariafe-r <mariamoya.fm@academia.umb.edu.co>
2026-08-05 00:21:56 -0500

    Delete taller-programacion-2026/taller-programacion-2026 directory

--- taller-programacion-2026/taller-programacion-2026/src/main/java/com/umb/taller/badsmell/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.umb.taller.badsmell;
-
-import com.umb.taller.repository.Repository;
-
-import java.util.Optional;
-
-public interface UserRepository extends Repository<User, String> {
-    Optional<User> findByEmail(String email);
-}
--- taller-programacion-2026/taller-programacion-2026/src/main/java/com/umb/taller/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.umb.taller.exception;
-
-public class BookNotFoundException extends DomainException {
-    public BookNotFoundException(String bookId) {
-        super("No se encontró el libro con id: " + bookId);
-    }
-}
